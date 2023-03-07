9fef3b82198e149d898bea987953b2fb9ad075e0
safiul-dev <parsonal494@gmail.com>
2023-03-07 14:58:43 +0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parser: "@typescript-eslint/parser",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react"],
+  rules: {},
+};
