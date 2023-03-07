e71c83e35b9b81c3171ec5c894515ae87384a4df
safiul-dev <parsonal494@gmail.com>
2023-03-08 00:36:48 +0600

    prisma client added with mysql database

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+let global: any;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (global.prisma != null) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
