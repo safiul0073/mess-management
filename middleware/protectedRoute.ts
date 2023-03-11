dc2a87eb80c969e7d61c23cfd1a3bb2ac59cd926
safiul-dev <parsonal494@gmail.com>
2023-03-12 01:03:03 +0600

    uncomment nessesary file

--- middleware/protectedRoute.ts
+++ middleware/protectedRoute.ts
@@ -3,27 +3,27 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 const protectedRoute = (handler: any) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    // try {
-    const authorization = req.headers.authorization;
+    try {
+      const authorization = req.headers.authorization;
 
-    const token = authorization?.split(" ")[1];
-    console.log(token);
-    if (!token) {
-      res.status(401).json({ ok: false, message: "Unauthorize!" });
-      return;
+      const token = authorization?.split(" ")[1];
+      console.log(token);
+      if (!token) {
+        res.status(401).json({ ok: false, message: "Unauthorize!" });
+        return;
+      }
+
+      const v = verify(token, process.env.ACCESS_TOKEN_SECRET ?? "");
+
+      if (!v) {
+        res.status(401).json({ ok: false, message: "Unauthorize!" });
+        return;
+      }
+
+      return handler(req, res);
+    } catch (e: any) {
+      res.status(401).send({ ok: false, message: e.message });
     }
-
-    const v = verify(token, process.env.ACCESS_TOKEN_SECRET ?? "");
-
-    if (!v) {
-      res.status(401).json({ ok: false, message: "Unauthorize!" });
-      return;
-    }
-
-    return handler(req, res);
-    // } catch (e: any) {
-    //   res.status(401).send({ ok: false, message: e.message });
-    // }
   };
 };
 
